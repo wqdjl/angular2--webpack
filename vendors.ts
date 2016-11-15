1e07c9784dd8088b50ffa3b508a16545fa2a6aa7
miao.deng <miaomiao618100@gmail.com>
2016-11-15 10:21:21 +0800

    upload learn angualr2+webpack project

---
+++ vendors.ts
@@ -0,0 +1,9 @@
+import '@angular/core';
+import '@angular/common';
+import '@angular/compiler';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
+
+import 'rxjs';
+ 
+import './src/assets/test.css';
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+let path = require('path');
+let webpack = require('webpack');
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+let config = {
+    entry: {
+        app: './src/app/main.ts',
+        polyfills: './polyfills.ts',
+        vendors: './vendors.ts'
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['', '.ts', '.js']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                loader: 'html',
+            },
+            {
+                test: /\.css$/,
+                //loaders: ['raw-loader'],
+                loader: 'raw',
+                include: path.resolve(__dirname, 'src/app')
+            },
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader'],
+                exclude: path.resolve(__dirname, 'src/app')
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file?name=assets/[name].[hash].[ext]'
+            },
+        ]
+    },
+    plugins: [
+        // new ExtractTextPlugin('[name].[hash].css'),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendors', 'polyfills'],
+            //filename:'build.[hash].js',
+            minChunks:2
+        }),
+    ]
+
+};
+
+module.exports = config;
+
